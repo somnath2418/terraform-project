3c3239b1420033a0981074bf25a2e1be9ea2a592
Som Nath <somnath23@gmail.com>
2026-05-12 22:13:51 +0530

    added static ip

--- modules/public-ip/main.tf
+++ modules/public-ip/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "this" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
   tags                = var.tags
 }
